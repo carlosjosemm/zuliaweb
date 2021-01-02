e4b3e9ee249ed4b9957f69a9ccc282a59cfe6aa8
Carlos Moncada <carlosjmoncadam@gmail.com>
2021-01-02 03:39:59 -0400

    first commit with next/chakra/ts

---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+    sm: '40em',
+    md: '52em',
+    lg: '64em',
+    xl: '80em',
+  })
