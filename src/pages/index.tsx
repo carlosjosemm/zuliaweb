4b16a58c376810da77aac0c8c761e36c50d9c5ae
Carlos Moncada <carlosjmoncadam@gmail.com>
2021-01-21 02:43:58 -0400

    added: 'head' component from next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Box, Center, IconButton, LightMode, Link, Spinner, useBreakpoint } from '@chakra-ui/react'
 import Header from '../components/Header'
 import HotProductSlide from '../components/HotProductSlide';
@@ -59,13 +60,24 @@ export default function Home() {
   return (
     <div className={styles.backgroundImg}>
     {console.log(br)}
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <title>Zulia Pa' Llevar☀️</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossOrigin="anonymous"
+        />
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css' rel='stylesheet' />
+        <meta
+          name="description"
+          content="Del Zulia para Buenos Aires!"
+        />
+      </Head>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+
       <Box maxW="1000px" /*h="100vh"*/ margin="auto" padding="0px" mb="0px" bgColor="#EBFAFF">
         {br? (br=='base')? <HeaderMobile /> : <Header /> : <Center><Spinner /></Center>}
         {/* {(br=='base')? <HeaderMobile /> : <Header /> } */}
