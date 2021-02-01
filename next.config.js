9da8f1bac1bcbde94be3ead4432acac67da58880
Carlos Moncada <carlosjmoncadam@gmail.com>
2021-01-31 21:15:12 -0400

    added: env variable for mercadolibre.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     publicRuntimeConfig: {
       NEXT_PUBLIC_GOOGLEMAPSKEY: process.env.NEXT_PUBLIC_GOOGLEMAPSKEY,
+      ENV_ML_ACCESSKEY: process.env.ENV_ML_ACCESSKEY,
     },
     env: {
         NEXT_PUBLIC_GOOGLEMAPSKEY: process.env.NEXT_PUBLIC_GOOGLEMAPSKEY,
+        ENV_ML_ACCESSKEY: process.env.ENV_ML_ACCESSKEY,
     }
 };
