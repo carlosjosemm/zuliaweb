101ecc2861d0922b360ba0831d9a85bb9323290b
Carlos Moncada <carlosjmoncadam@gmail.com>
2021-02-01 01:45:35 -0400

    fixed: GC key passed to env var.

--- src/pages/api/checkout.js
+++ src/pages/api/checkout.js
@@ -1,15 +1,26 @@
 import mercadopago from 'mercadopago';
 import {v4} from 'uuid';
+console.log('trying to require admin...')
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../data/tiendazuliaweb-c2d6d6398f0e.json');
+// admin.app().delete().then(() => console.log('Firebase app deleted successfully')).catch((error) => console.log(error));
+// const serviceAccount = require('../../../data/tiendazuliaweb-c2d6d6398f0e.json');
+// console.log('trying to initialize...')
+    // admin.initializeApp({
+    // credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_CLOUD_KEY_JSON))
+    // })
 
-console.log('FIREBASE INSTANCE ID: ', admin.app().instanceId);
-if(!admin.app().instanceId) { 
+// console.log('FIREBASE INSTANCE ID: ', admin.app().instanceId);
+// console.log('admin name: ', admin.app.name)
+if (admin.app.length==1) { 
+    console.log('trying to initialize...')
     admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_CLOUD_KEY_JSON))
     })
+    console.log('lenght after init: ', admin.app.length)
 };
+console.log('trying to create db...')
 const db = admin.firestore();
+ console.log('ENV: ', process.env.NODE_ENV);
 
 export default (req, res) => {
     const configAccess_token = process.env.ENV_ML_ACCESSKEY;
@@ -43,15 +54,15 @@ export default (req, res) => {
                 outcome: data.body.status,
                 detail: data.body.status_detail,
             });
-            res.redirect(`/transaction/${token}`);
+            res.redirect(`${(process.env.NODE_ENV=='production')? 'https://zuliaweb.vercel.app/transaction' : '/transaction' }/${token}`);
         } else {
             const badtoken = v4();
-            res.redirect(`/transaction/${badtoken}`);
+            res.redirect(`${(process.env.NODE_ENV=='production')? 'https://zuliaweb.vercel.app/transaction' : '/transaction' }/${badtoken}`);
         }
     }).catch( (error) => {
         // ...
         console.error(error)
         const badtoken = v4();
-        res.redirect(`/transaction/${badtoken}`);
+        res.redirect(`https://zuliaweb.vercel.app/transaction/${badtoken}`);
     });
 }
