03c22cc976cd34f7b72503b6d1739a5e0c9ae560
Carlos Moncada <carlosjmoncadam@gmail.com>
2021-02-01 01:41:10 -0400

    added: env var for GC.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@ module.exports = {
     publicRuntimeConfig: {
       NEXT_PUBLIC_GOOGLEMAPSKEY: process.env.NEXT_PUBLIC_GOOGLEMAPSKEY,
       ENV_ML_ACCESSKEY: process.env.ENV_ML_ACCESSKEY,
+      GOOGLE_CLOUD_KEY_JSON: process.env.GOOGLE_CLOUD_KEY_JSON,
     },
     env: {
         NEXT_PUBLIC_GOOGLEMAPSKEY: process.env.NEXT_PUBLIC_GOOGLEMAPSKEY,
         ENV_ML_ACCESSKEY: process.env.ENV_ML_ACCESSKEY,
+        GOOGLE_CLOUD_KEY_JSON: process.env.GOOGLE_CLOUD_KEY_JSON,
     }
 };
